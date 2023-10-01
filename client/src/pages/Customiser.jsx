76908fc01a8fef05999fb3ca352968c6daabad0c
Ayush Awasthi <ayush.awasthi468@gmail.com>
2023-10-02 01:01:41 +0530

    Download button

--- client/src/pages/Customiser.jsx
+++ client/src/pages/Customiser.jsx
@@ -160,11 +160,18 @@ const Customiser = () => {
                 handleClick={() => handleActiveFilterTab(tab.name)}
               />
             ))}
+           <button className='download-btn' onClick={downloadCanvasToImage}>
+              <img
+                src={download}
+                alt='download_image'
+                className='w-3/5 h-3/5 object-contain'
+              />
+            </button>
           </motion.div>
         </>
       )}
     </AnimatePresence>
-  );
-};
+  )
+}
 
 export default Customiser;
